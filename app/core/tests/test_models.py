04e47cfb9e176480c5ad0f8ac886b064085f65bd
Behnam <behnams_id@yahoo.com>
2020-02-27 17:50:58 +0330

    wait_for_db and configure docker-compose.yml

--- app/core/tests/test_models.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from django.contrib.auth import get_user_model
-
-
-class ModelTests(TestCase):
-    def test_new_use_invalid_email(self):
-        with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(None, 'test123')
